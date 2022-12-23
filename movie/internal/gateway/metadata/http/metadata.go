62080dbcbd9e2faab285ce4d9fb493d39725ffae
jszafran <jszafran.pv@gmail.com>
2022-12-23 08:28:20 +0100

    Add Consul registry for metadata service.

--- movie/internal/gateway/metadata/http/metadata.go
+++ movie/internal/gateway/metadata/http/metadata.go
@@ -4,23 +4,33 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+	"math/rand"
 	model "movieexample.com/metadata/pkg/model"
 	"movieexample.com/movie/internal/gateway"
+	"movieexample.com/pkg/discovery"
 	"net/http"
 )
 
 // Gateway defines a movie metadata HTTP gateway.
 type Gateway struct {
-	addr string
+	registry discovery.Registry
 }
 
 // New creates a new HTTP gateway for a movie metadata service.
-func New(addr string) *Gateway {
-	return &Gateway{addr}
+func New(registry discovery.Registry) *Gateway {
+	return &Gateway{registry: registry}
 }
 
 func (g *Gateway) Get(ctx context.Context, id string) (*model.Metadata, error) {
-	req, err := http.NewRequest(http.MethodGet, g.addr+"/metadata", nil)
+	addrs, err := g.registry.ServiceAddresses(ctx, "metadata")
+	if err != nil {
+		return nil, err
+	}
+
+	url := "http://" + addrs[rand.Intn(len(addrs))] + "/metadata"
+	log.Printf("Calling metadata service. Request: GET " + url)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -29,7 +39,6 @@ func (g *Gateway) Get(ctx context.Context, id string) (*model.Metadata, error) {
 	values := req.URL.Query()
 	values.Add("id", id)
 	req.URL.RawQuery = values.Encode()
-
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
@@ -41,7 +50,6 @@ func (g *Gateway) Get(ctx context.Context, id string) (*model.Metadata, error) {
 	} else if resp.StatusCode/100 != 2 {
 		return nil, fmt.Errorf("non-2xx response: %v", resp)
 	}
-
 	var v *model.Metadata
 	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
 		return nil, err
